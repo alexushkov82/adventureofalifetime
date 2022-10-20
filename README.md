f9b79cf8357bd8ce17ae22db0f802cec5a6c78a5
Alex Ushkov <alexushkov82@gmail.com>
2022-10-20 18:02:26 +0300

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+#adventureofalifetime
+
+Coldplay - Adventure of a Lifetime (Live from Spotify London)
---
+++ android/app/src/main/kotlin/com/example/adventureofalifetime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.adventureofalifetime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
