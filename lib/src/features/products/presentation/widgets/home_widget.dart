eabced3346e7935e5a69a1f78baed8dd91ae1ddc
Alex Ushkov <alexushkov82@gmail.com>
2025-06-01 20:29:44 +0300

    ref: create product widget

--- lib/src/features/products/presentation/widgets/home_widget.dart
+++ lib/src/features/products/presentation/widgets/home_widget.dart
@@ -1,7 +1,6 @@
+import 'package:adventureofalifetime/src/features/products/presentation/widgets/product_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import '../../../../config/router/routes.dart';
 import '../../../../di/di.dart';
 import '../bloc/products_bloc/products_bloc.dart';
 
@@ -33,43 +32,7 @@ class HomeWidget extends StatelessWidget {
                     itemCount: state.products.length,
                     itemBuilder: (context, index) {
                       final product = state.products[index];
-                      return GestureDetector(
-                        onTap: () {
-                          context.push(AppRoutes.item.replaceFirst(':productId', product.id));
-                        },
-                        child: Card(
-                          child: Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Expanded(
-                                  child: Image.network(product.image ?? '', fit: BoxFit.fitHeight),
-                                ),
-                                SizedBox(height: 10),
-                                Text(
-                                  product.title,
-                                  textAlign: TextAlign.center,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                SizedBox(height: 10),
-                                Text(
-                                  '\$${product.price}',
-                                  style: TextStyle(fontSize: 20, color: Colors.green),
-                                ),
-                                SizedBox(height: 10),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    print('On Add to Cart');
-                                  },
-                                  child: Text('Add to Cart'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
+                      return ProductWidget(product: product);
                     },
                   );
                 } else {
